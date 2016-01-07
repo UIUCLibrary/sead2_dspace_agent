5899af1bfb1e3c8b7309abcb60f3ead577215fb1
Bill Ingram <waingram@gmail.com>
2016-01-07 15:10:46 -0600

    initial commit

---
+++ lib/sead2_dspace_agent.rb
@@ -0,0 +1,5 @@
+require "sead2_dspace_agent/version"
+
+module Sead2DspaceAgent
+  # Your code goes here...
+end
---
+++ spec/sead2_dspace_agent_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Sead2DspaceAgent do
+  it 'has a version number' do
+    expect(Sead2DspaceAgent::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
