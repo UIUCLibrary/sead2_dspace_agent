c461a6be79c2acb3546d8d72c3c8d5d31568d96f
Nushrat Jahan Khan <njkhan2@illinois.edu>
2016-02-12 10:34:03 -0600

    metadata collection updated for new ORE map

--- lib/sead2_dspace_agent/research_object.rb
+++ lib/sead2_dspace_agent/research_object.rb
@@ -17,19 +17,19 @@ module Sead2DspaceAgent
       @metadata             = {}
       @metadata[:id]        = ore["describes"]["@id"]    # Don't add this id in metadata
       @metadata[:title]     = ore["describes"]["Title"]
-      @metadata[:alt_title] = ore["describes"]["Alternative Title"]
-      @metadata[:abstract]  = ore["describes"]["Abstract"]
+      @metadata[:alt_title] = ore["describes"]["Alternative Title"]*", "
+      @metadata[:abstract]  = ore["describes"]["Abstract"]*", "
       @metadata[:rights]    = ore["Rights"]
-      @metadata[:creator]   = ore["describes"]["Creator"]
+      @metadata[:creator]   = ore["describes"]["Uploaded By"]
       @metadata[:date]      = ore["describes"]["Creation Date"]
       # @metadata[:has_part]  = ore["describes"]["Has Part"]  # Only includes the agg resources' id
-      @metadata[:subject]   = ore["describes"]["Keywords"]
+      @metadata[:subject]   = ore["describes"]["Keywords"]*", "
 
       other_info                          = {}
-      other_info["Funding Institution"]   = ore["describes"]["Funding Institution"]
-      other_info["Time Period"]           = ore["describes"]["Time Periods"]
-      other_info["Audience"]              = ore["describes"]["Audience"]
-      other_info["Project Investigators"] = ore["describes"]["Principal Investigator(s)"]
+      other_info["Funding Institution"]   = ore["describes"]["Funding Institution"]*", "
+      other_info["Time Period"]           = ore["describes"]["Time Periods"]*", "
+      other_info["Audience"]              = ore["describes"]["Audience"]*", "
+      other_info["Project Investigators"] = ore["describes"]["Principal Investigator(s)"]*", "
       @metadata[:description] = other_info.map{|k,v| "#{k} = #{v}"}.join('; ')
 
       @all_metadata = Array.new
