304fef1ed39508110a118b4e1ffe0eacb285b3ed
Nushrat Jahan Khan <njkhan2@illinois.edu>
2016-02-29 16:29:33 -0600

    non-DC terms covered

--- lib/sead2_dspace_agent/research_object.rb
+++ lib/sead2_dspace_agent/research_object.rb
@@ -21,41 +21,15 @@ module Sead2DspaceAgent
       @metadata[:date]      = ore["describes"]["Creation Date"]
 
 
-      # def join_values(k, a)
-      #   if a.nil?
-      #     return
-      #   elsif a.kind_of?(Array) && !a.nil?
-      #     @other_info[k] = a*", "
-      #   elsif a.kind_of?(String) && !a.nil?
-      #     @other_info[k] = a
-      #   end
-      # end
-      #
-      # # Get all the fields that cannot be directly mapped to DC terms
-      # @other_info = {}
-      # join_values('Uploaded By', ore["describes"]["Uploaded By"])
-      # join_values('Funding Institution', ore["describes"]["Funding Institution"])
-      # join_values('Grant Number', ore["describes"]["Grant Number"])
-      # join_values('Publisher', ore["describes"]["Publisher"])
-      # join_values('Time Period', ore["describes"]["Time Periods"])
-      # join_values('Project Investigators', ore["describes"]["Principal Investigator(s)"])
-      # join_values('Contact', ore["describes"]["Contact"])
-      # join_values('Audience', ore["describes"]["Audience"])
-      # join_values('Bibliographic Citation', ore["describes"]["Bibliographic Citation"])
-      # join_values('Related Publications', ore["describes"]["Related Publications"])
-
-      # @other_info["Uploaded By"]           = ore["describes"]["Uploaded By"]
-      # @other_info["Funding Institution"]   = ore["describes"]["Funding Institution"]
-      # @other_info["Grant Number"]          = ore["describes"]["Grant Number"]
-      # @other_info["Publisher"]             = ore["describes"]["Publisher"]
-      # @other_info["Time Period"]           = ore["describes"]["Time Periods"]
-      # @other_info["Project Investigators"] = ore["describes"]["Principal Investigator(s)"]
-      # @other_info["Contact"]               = ore["describes"]["Contact"]
-      # @other_info["Audience"]              = ore["describes"]["Audience"]
-      # @other_info["Bibliographic Citation"]= ore["describes"]["Bibliographic Citation"]
-      # @other_info["Related Publications"]  = ore["describes"]["Related Publications"]
-      # @metadata[:description] = @other_info.map{|k,v| "#{k}: #{v}"}.join('; ')
-
+      def join_values(k, a)
+        if a.nil?
+          return
+        elsif a.kind_of?(Array) && !a.nil?
+          @other_info[k] = a*", "
+        elsif a.kind_of?(String) && !a.nil?
+          @other_info[k] = a
+        end
+      end
 
       # Create separate hash for each subjects and creators
       def mult_values(keys, arrays)
@@ -65,6 +39,21 @@ module Sead2DspaceAgent
           end
         end
       end
+
+      # Get all the fields that cannot be directly mapped to DC terms
+      @other_info = {}
+      join_values('Uploaded By', ore["describes"]["Uploaded By"])
+      join_values('Funding Institution', ore["describes"]["Funding Institution"])
+      join_values('Grant Number', ore["describes"]["Grant Number"])
+      join_values('Publisher', ore["describes"]["Publisher"])
+      join_values('Time Period', ore["describes"]["Time Periods"])
+      join_values('Project Investigators', ore["describes"]["Principal Investigator(s)"])
+      join_values('Contact', ore["describes"]["Contact"])
+      join_values('Audience', ore["describes"]["Audience"])
+      join_values('Bibliographic Citation', ore["describes"]["Bibliographic Citation"])
+      join_values('Related Publications', ore["describes"]["Related Publications"])
+      @metadata[:description] = @other_info.map{|k,v| "#{k}: #{v}"}.join('; ')
+
 
       @collect    = Array.new
       @sub        = ore["describes"]["Keywords"]
