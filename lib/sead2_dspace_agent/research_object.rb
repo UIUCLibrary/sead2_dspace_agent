6e9c87e1f4e436e881f6a3aacea73bea67569464
Nushrat Jahan Khan <njkhan2@illinois.edu>
2016-03-07 15:30:52 -0600

    metadata parsing methods reorganized and updated with crash handling for parsing failure

--- lib/sead2_dspace_agent/research_object.rb
+++ lib/sead2_dspace_agent/research_object.rb
@@ -16,11 +16,9 @@ module Sead2DspaceAgent
 
       @metadata             = {}
       @metadata[:id]        = ore["describes"]["@id"]    # Don't add this id in metadata
-      @metadata[:title]     = ore["describes"]["Title"]
-      @metadata[:rights]    = ore["Rights"]
-      @metadata[:date]      = ore["describes"]["Creation Date"]
 
 
+      # join values of non-dc terms to post as single value for description
       def join_values(k, a)
         if a.nil?
           return
@@ -31,62 +29,68 @@ module Sead2DspaceAgent
         end
       end
 
-      # Create separate hash for each subjects and creators
+      # Create separate hash for each dc terms
       def mult_values(keys, values)
         if values.nil?
           return
         elsif values.is_a? Array
           values.each do |i|
-            @collect << {'key' => keys, 'value' => i, 'language' => 'eng'}
+            @all_metadata << {'key' => keys, 'value' => i, 'language' => 'eng'}
           end
         elsif values.is_a? String
-          @collect << {'key' => keys, 'value' => values, 'language' => 'eng'}
+          @all_metadata << {'key' => keys, 'value' => values, 'language' => 'eng'}
         else
           return
         end
       end
 
+
       # Get all the fields that cannot be directly mapped to DC terms
+      @all_metadata    = Array.new
       @other_info = {}
-      join_values('Uploaded By', ore["describes"]["Uploaded By"])
-      join_values('Funding Institution', ore["describes"]["Funding Institution"])
-      join_values('Grant Number', ore["describes"]["Grant Number"])
-      join_values('Publisher', ore["describes"]["Publisher"])
-      join_values('Time Period', ore["describes"]["Time Periods"])
-      join_values('Project Investigators', ore["describes"]["Principal Investigator(s)"])
-      join_values('Contact', ore["describes"]["Contact"])
-      join_values('Audience', ore["describes"]["Audience"])
-      join_values('Bibliographic Citation', ore["describes"]["Bibliographic Citation"])
-      join_values('Related Publications', ore["describes"]["Related Publications"])
-      @metadata[:description] = @other_info.map{|k,v| "#{k}: #{v}"}.join('; ')
+      begin
+        join_values('Uploaded By', ore["describes"]["Uploaded By"])
+        join_values('Funding Institution', ore["describes"]["Funding Institution"])
+        join_values('Grant Number', ore["describes"]["Grant Number"])
+        join_values('Publisher', ore["describes"]["Publisher"])
+        join_values('Time Period', ore["describes"]["Time Periods"])
+        join_values('Project Investigators', ore["describes"]["Principal Investigator(s)"])
+        join_values('Contact', ore["describes"]["Contact"])
+        join_values('Audience', ore["describes"]["Audience"])
+        join_values('Bibliographic Citation', ore["describes"]["Bibliographic Citation"])
+        join_values('Related Publications', ore["describes"]["Related Publications"])
+        description = @other_info.map{|k,v| "#{k}: #{v}"}.join('; ')
+      rescue => e
+        "#{e.message}"
+      end
 
+      @all_metadata << {'key' => 'dc.description', 'value' => description, 'language' => 'eng'}
 
-      @collect    = Array.new
       @sub        = ore["describes"]["Keywords"]
       @creator    = ore["describes"]["Creator"]
       @alt_title  = ore["describes"]["Alternative Title"]
       @abstract   = ore["describes"]["Abstract"]
       @time       = ore["describes"]["Start/End Date"]
       @references = ore["describes"]["References"]
+      @title      = ore["describes"]["Title"]
+      @rights     = ore["Rights"]
+      @date       = ore["describes"]["Creation Date"]
 
-      mult_values('dc.subject', @sub)
-      mult_values('dc.creator', @creator)
-      mult_values('dc.title.alternative', @alt_title)
-      mult_values('dc.description.abstract', @abstract)
-      mult_values('dc.coverage.temporal', @time)
-      mult_values('dc.relation.references', @references)
-
-
-      @all_metadata = Array.new
-      @all_metadata.concat(@collect)
-      keys = %w[dc.title dc.description dc.date dc.rights]
-      values = [@metadata[:title], @metadata[:description], @metadata[:date], @metadata[:rights]]
-
-      keys.zip(values).each do|i, j|
-        @all_metadata << {'key'=> i , 'value'=> j , 'language' => 'eng'}
+      begin
+        mult_values('dc.subject', @sub)
+        mult_values('dc.creator', @creator)
+        mult_values('dc.title.alternative', @alt_title)
+        mult_values('dc.description.abstract', @abstract)
+        mult_values('dc.coverage.temporal', @time)
+        mult_values('dc.relation.references', @references)
+        mult_values('dc.title', @title)
+        mult_values('dc.rights', @rights)
+        mult_values('dc.date', @date)
+      rescue => e
+        "#{e.message}"
       end
 
-
+      # Metadata for aggregated resources
       ars                   = ore["describes"]["aggregates"]
       @aggregated_resources = ars.map { |ar| AggregatedResource.new ar }
     end
