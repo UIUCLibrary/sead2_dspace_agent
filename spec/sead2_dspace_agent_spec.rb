2a5530033555af53729fa38d6fe6f3f3522d64f6
Bill Ingram <waingram@gmail.com>
2016-03-10 16:41:37 -0600

    test dspace item update

--- spec/sead2_dspace_agent_spec.rb
+++ spec/sead2_dspace_agent_spec.rb
@@ -4,8 +4,16 @@ describe Sead2DspaceAgent do
   it 'has a version number' do
     expect(Sead2DspaceAgent::VERSION).not_to be nil
   end
+end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+describe Sead2DspaceAgent::DspaceConnection do
+
+  describe '.update_item_bitstream' do
+    it 'streams the file' do
+      @dscon = Sead2DspaceAgent::DspaceConnection.new
+      @dscon.create_item 1
+      expect(@dscon.update_item_bitstream('AMQP Messaging System.pptx', 'https://sead2.ncsa.illinois.edu/api/files/568c35b2e4b04a0a87bcce17/blob?key=0LBmIgHgPS', 129707)).not_to be nil
+    end
   end
+
 end
